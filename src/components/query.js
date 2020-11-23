c58e88c02d2eb22b472379bb91bc5a72d11f36f6
eric_ <eric_@DESKTOP-E06PVB6>
2020-11-23 11:54:58 -0500

    new branch with query

---
+++ src/components/query.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function query() {
+    const [cpeData, setCPEData] = useState([]);
+  
+    const name = 'Microsoft Word'
+    
+    useEffect(() => {
+        fetch('/getCPE?cpe='+name).then(res => res.json()).then(data =>{
+            //setCPEData(data.cpe.cpes)
+            console.log(data.cpe.cpes);
+        });
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
