72b3e0d7db8bf47a7ec4a55ed8673c627fdc2d37
eric_ <eric_@DESKTOP-E06PVB6>
2020-11-24 02:38:00 -0500

    made query proper syntax

--- src/components/query.js
+++ src/components/query.js
@@ -4,9 +4,10 @@ export default function query() {
     const [cpeData, setCPEData] = useState([]);
   
     const name = 'Microsoft Word'
-    
+    const version = '2.3'
+
     useEffect(() => {
-        fetch('/getCPE?search='+name).then(res => res.json()).then(data =>{
+        fetch('/getCPE?search='+name+'&version='+version).then(res => res.json()).then(data =>{
             //setCPEData(data.cpe.cpes)
             console.log(data.cpe.cpes);
         });
